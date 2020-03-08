fca43dac722457c471466ed4b48300aaab575c6c
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-03-08 18:39:23 -0500

    set up posts component

---
+++ starwars/src/components/Posts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Posts = props => {
+    const Container = styled.div`
+    width: 50%;
+    margin: 0 auto;
+    padding: 10px 30px;
+    background: black;
+    opacity: 0.8;
+    li{
+        color: white;
+        list-style: none;
+        font-size: 30px;
+    }
+    `
+return (
+    <Container>
+        <ul>
+            <li>{props.props.name} was born in {props.props.birth_year}</li>
+        </ul>
+    </Container>
+)
+
+}
+
+export default Posts;
